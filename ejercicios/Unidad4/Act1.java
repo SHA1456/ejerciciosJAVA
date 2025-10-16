dda7eed2dc63e289f433210c767b2e3dd8d8628a
SHA1456 <heredia.acero.sergio@iescamas.es>
2025-10-16 20:00:48 +0200

    Add files via upload

---
+++ ejercicios/Unidad4/Act1.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Act1 {
+
+	public static void main(String[] args) {
+		
+		eco(5);
+	}
+	
+	public static void eco (int n) {
+		int i=0;
+		
+		while(i<n) {
+			
+			System.out.println("Eco... ");
+			i++;
+			
+		}
+		
+	}
+
+}
