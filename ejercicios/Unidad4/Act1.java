849e59a4c412ec532d85138b41acde0b503f6759
SHA1456 <heredia.acero.sergio@iescamas.es>
2025-10-17 17:55:45 +0200

    Add files via upload

---
+++ ejercicios/Unidad4/Act1.java
@@ -0,0 +1,26 @@
+package com;
+
+public class Act1 {
+
+	public static void main(String[] args) {
+		
+		eco(5);
+	}
+	
+		/**
+		 * Funcion que imprime "eco..." n veces
+		 * @param n Número de veces que se repite
+		 */
+	public static void eco (int n) {
+		int i=0;
+		
+		while(i<n) {
+			
+			System.out.println("Eco... ");
+			i++;
+			
+		}
+		
+	}
+
+}
