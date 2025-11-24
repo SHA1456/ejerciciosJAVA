58eb1d39fdfffed8956d26dc14d6549cc8530613
SHA1456 <heredia.acero.sergio@iescamas.es>
2025-11-24 18:08:07 +0100

    Add files via upload

---
+++ ejercicios/Unidad6/Act14.java
@@ -0,0 +1,60 @@
+package com;
+import java.util.Scanner;
+import java.util.Random;
+public class Act14 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random ();
+		String frase;
+		
+		System.out.println("Introduce una frase o palabra: ");
+		frase=sc.nextLine();
+		
+		if (frase.length()==0) {
+			
+			System.err.println("No has introducido una frase o palabra válida");
+			
+		}else {
+			String anagrama = Anagrama(frase);
+			
+			System.out.println("Anagrama generado: " + anagrama);
+			
+			String intento;
+			do {
+				
+			System.out.println("Intenta adivinar la palabra original: ");
+			intento=sc.nextLine();
+			
+			if(!intento.equalsIgnoreCase(frase)) {
+				System.out.println("Incorrecto, inténtalo de nuevo");
+			}
+			
+			}while (!intento.equalsIgnoreCase(frase));
+			
+			System.out.println("¡Enhorabuena! Has acertado");
+		}
+		
+		
+	}
+	
+	public static String Anagrama (String frase) {
+			
+			char [] letras = frase.toCharArray();
+			Random random = new Random ();
+			
+			for(int i=0;i<letras.length;i++) {
+				
+				int posicionaleatoria=random.nextInt(letras.length);
+				
+				//Intercambiar
+				char temp = letras[i];
+				letras[i]=letras[posicionaleatoria];
+				letras[posicionaleatoria]=temp;
+			}
+
+		return new String (letras);
+		
+	}
+
+}
