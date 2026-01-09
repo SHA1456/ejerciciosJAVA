81e2546349d1ccc1e2d7876a4052a893ad0db37f
SHA1456 <heredia.acero.sergio@iescamas.es>
2026-01-09 18:42:26 +0100

    Add files via upload

---
+++ ejercicios/Unidad7/Calculadora/CalculadoraTest.java
@@ -0,0 +1,47 @@
+package es.calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	Calculadora calculadora;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@DisplayName ("División con uno de los digitos siendo cero")
+	void testDividirconCeros() {
+		calculadora = new Calculadora (1,0);
+		calculadora.dividir();
+	}
+	
+	@Test
+	void test() {
+		
+	}
+	
+	
+	
+
+}
