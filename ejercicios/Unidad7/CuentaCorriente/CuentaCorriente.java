fff5d6da3e96165fbcdaccd06657477c3fd94815
SHA1456 <heredia.acero.sergio@iescamas.es>
2026-01-13 20:57:14 +0100

    Update CuentaCorriente.java

--- ejercicios/Unidad7/CuentaCorriente/CuentaCorriente.java
+++ ejercicios/Unidad7/CuentaCorriente/CuentaCorriente.java
@@ -1,4 +1,4 @@
-package com;
+package es.programacion;
 
 public class CuentaCorriente {
 
@@ -12,6 +12,12 @@ public class CuentaCorriente {
 	
 	private Banco nombrebanco;
 	
+	public CuentaCorriente (String nombre,String DNI) {
+		this.nombre=nombre;
+		this.DNI=DNI;
+		this.limite=-50.0;
+		this.saldo=0.0;
+	}
 	
 	public CuentaCorriente (String nombre, String DNI, Banco nombrebanco) {
 		
@@ -21,11 +27,11 @@ public class CuentaCorriente {
 		
 		saldo=0.0;
 		
-		limite=-50.0;
+		this.limite=-50.0;
 		
 		this.nombrebanco=nombrebanco;
 	}
-	
+ 	
 	public CuentaCorriente (double saldo) {
 		
 		this.saldo=saldo;
@@ -44,8 +50,10 @@ public class CuentaCorriente {
 	public boolean sacarDinero (double cantidad) {
 		
 		if (this.saldo-cantidad>=this.limite) {
+			this.saldo=this.saldo-cantidad;
 			return true;
 		}else {
+			System.out.println("Has superado el límite de descubierto");
 			return false;
 		}
 	}
@@ -65,18 +73,43 @@ public class CuentaCorriente {
 		System.out.println("El nombre es: " + nombre + ", el DNI es: " + DNI + ", el saldo es: " + saldo + " y el límite de descubierto es: " + limite);
 	}
 
-	public  Banco getNombrebanco() {
+	
+	
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Banco getNombrebanco() {
 		return nombrebanco;
 	}
 
-	public  void setNombrebanco(Banco nombrebanco) {
+	public void setNombrebanco(Banco nombrebanco) {
 		this.nombrebanco = nombrebanco;
 	}
-
-
-	
-	
-	
-	
 	
 }
+
