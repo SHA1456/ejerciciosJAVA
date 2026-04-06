69f1036d7483d35f3a057ba9a60202828b9d7ca0
Sergio <heredia.acero.sergio@iescamas.es>
2026-04-06 18:44:48 +0200

    Actualizacion U10

--- Unidad10/src/main/java/es/iescamas/programacion/ej6/Ejercicio6.java
+++ Unidad10/src/main/java/es/iescamas/programacion/ej6/Ejercicio6.java
@@ -1,5 +1,5 @@
 package es.iescamas.programacion.ej6;
-
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -9,17 +9,21 @@ import java.io.IOException;
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 
 		try (
 				BufferedReader br = new BufferedReader(new FileReader("firmas.txt"))){
 			String linea;
 			
 			while ((linea = br.readLine()) != null) {
-                System.out.println(linea);
+                System.out.println("Firmas: \n" + linea);
+                String[] nombres = new String [linea.length()];
             }
 			
-			System.out.println("¿Quiéres añadir una nueva firma?");
+			System.out.println("Añade una nueva firma: ");
+			String firma=sc.nextLine();
+		
+			
 			
 			
 		}catch (IOException e) {
